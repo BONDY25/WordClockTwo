7dd0534893849615c1642664c9d86cd008e85e31
BONDY25 <aiden.bond1593@gmail.com>
2025-05-16 08:16:36 +0100

    Add project files.

---
+++ WordClockTwo/ClockForm.Designer.cs
@@ -0,0 +1,78 @@
+﻿namespace WordClockTwo
+{
+    partial class ClockForm
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(ClockForm));
+            lblTime = new Label();
+            lblDate = new Label();
+            SuspendLayout();
+            // 
+            // lblTime
+            // 
+            lblTime.Anchor = AnchorStyles.Top | AnchorStyles.Bottom | AnchorStyles.Left | AnchorStyles.Right;
+            lblTime.Font = new Font("Impact", 70F, FontStyle.Regular, GraphicsUnit.Point);
+            lblTime.ForeColor = Color.White;
+            lblTime.Location = new Point(12, 34);
+            lblTime.Name = "lblTime";
+            lblTime.Size = new Size(1819, 864);
+            lblTime.TabIndex = 0;
+            lblTime.Text = "WORD CLOCK TWO";
+            lblTime.TextAlign = ContentAlignment.MiddleCenter;
+            // 
+            // lblDate
+            // 
+            lblDate.Font = new Font("Impact", 12F, FontStyle.Regular, GraphicsUnit.Point);
+            lblDate.ForeColor = Color.White;
+            lblDate.Location = new Point(12, 9);
+            lblDate.Name = "lblDate";
+            lblDate.Size = new Size(383, 25);
+            lblDate.TabIndex = 1;
+            lblDate.Text = "Date";
+            // 
+            // ClockForm
+            // 
+            AutoScaleDimensions = new SizeF(6F, 16F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackColor = Color.Black;
+            ClientSize = new Size(1843, 907);
+            Controls.Add(lblDate);
+            Controls.Add(lblTime);
+            Font = new Font("Impact", 9F, FontStyle.Regular, GraphicsUnit.Point);
+            Icon = (Icon)resources.GetObject("$this.Icon");
+            Name = "ClockForm";
+            Text = "WordClockTwo";
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private Label lblTime;
+        private Label lblDate;
+    }
+}
---
+++ WordClockTwo/ClockForm.cs
@@ -0,0 +1,212 @@
+using System.Windows.Forms;
+
+namespace WordClockTwo
+{
+    public partial class ClockForm : Form
+    {
+
+        private System.Windows.Forms.Timer _timer;
+        public int mode = 1;
+
+        public ClockForm()
+        {
+            InitializeComponent();
+            UpdateTime();
+            StartTimer();
+        }
+
+        // Start Timer----------------------------------------------------------------------------------
+        private void StartTimer()
+        {
+            // Create a timer that ticks every second
+            _timer = new System.Windows.Forms.Timer();
+            _timer.Interval = 1000; // 1 second
+            _timer.Tick += (s, e) =>
+            {
+                if (DateTime.Now.Second == 0) // only update on new minute
+                {
+                    UpdateTime();
+                }
+            };
+
+            _timer.Start();
+        }
+
+        // Update Time Label ----------------------------------------------------------------------------------
+        private async void UpdateTime()
+        {
+            string newTime = ConvertTimeToWords(DateTime.Now);
+            await FadeLabelAsync(lblTime, newTime);
+            lblDate.Text = ConvertDateToWords(DateTime.Now); ;
+        }
+
+        // Fade Method 1/2----------------------------------------------------------------------------------
+        private async Task FadeLabelAsync(Label label, string newText, int steps = 20, int interval = 25)
+        {
+            Color startColor = label.ForeColor;
+            Color endColor = Color.FromArgb(0, 0, 0, 0); // Transparent color
+
+            // Fade out
+            for (int i = 0; i < steps; i++)
+            {
+                float fraction = (float)i / (steps - 1);
+                label.ForeColor = InterpolateColor(startColor, endColor, fraction);
+                await Task.Delay(interval);
+            }
+
+            label.Text = newText;
+
+
+            // Fade in
+            for (int i = 0; i < steps; i++)
+            {
+                float fraction = (float)i / (steps - 1);
+                label.ForeColor = InterpolateColor(endColor, startColor, fraction);
+                await Task.Delay(interval);
+            }
+        }
+
+        // Fade Method 2/2----------------------------------------------------------------------------------
+        private Color InterpolateColor(Color start, Color end, float fraction)
+        {
+            int r = (int)(start.R + (end.R - start.R) * fraction);
+            int g = (int)(start.G + (end.G - start.G) * fraction);
+            int b = (int)(start.B + (end.B - start.B) * fraction);
+            return Color.FromArgb(r, g, b);
+        }
+
+        // Convert time to words ----------------------------------------------------------------------------------
+        private string ConvertTimeToWords(DateTime time)
+        {
+            string timeString = "";
+
+            var numbersToWords = new Dictionary<int, string>
+            {
+                [0] = "Twelve",
+                [1] = "One",
+                [2] = "Two",
+                [3] = "Three",
+                [4] = "Four",
+                [5] = "Five",
+                [6] = "Six",
+                [7] = "Seven",
+                [8] = "Eight",
+                [9] = "Nine",
+                [10] = "Ten",
+                [11] = "Eleven",
+                [12] = "Twelve",
+                [13] = "Thirteen",
+                [14] = "Fourteen",
+                [15] = "Fifteen",
+                [16] = "Sixteen",
+                [17] = "Seventeen",
+                [18] = "Eighteen",
+                [19] = "Nineteen",
+                [20] = "Twenty",
+                [30] = "Thirty",
+                [40] = "Forty",
+                [50] = "Fifty"
+            };
+
+            string NumberToWords(int num)
+            {
+                if (numbersToWords.ContainsKey(num)) return numbersToWords[num];
+                return numbersToWords[num / 10 * 10] + "-" + numbersToWords[num % 10];
+            }
+
+            string TimePeriod(int hour)
+            {
+                if (hour < 12) return "In The Morning";
+                else if (hour < 17) return "In The Afternoon";
+                else if (hour < 20) return "In The Evening";
+                else return "At Night";
+            }
+
+            int hour = time.Hour;
+            int minute = time.Minute;
+            string period = TimePeriod(hour);
+
+            if (mode == 1)
+            {
+                if (minute == 0)
+                {
+                    timeString = $"{NumberToWords(hour % 12 == 0 ? 12 : hour % 12)} O'Clock {period}";
+                }
+                else if (minute == 15)
+                {
+                    timeString = $"Quarter Past {NumberToWords(hour % 12 == 0 ? 12 : hour % 12)} {period}";
+                }
+                else if (minute == 30)
+                {
+                    timeString = $"Half Past {NumberToWords(hour % 12 == 0 ? 12 : hour % 12)} {period}";
+                }
+                else if (minute == 45)
+                {
+                    timeString = $"Quarter To {NumberToWords((hour + 1) % 12 == 0 ? 12 : (hour + 1) % 12)} {period}";
+                }
+                else if (minute < 30)
+                {
+                    timeString =
+                        $"{NumberToWords(minute)} Minutes Past {NumberToWords(hour % 12 == 0 ? 12 : hour % 12)} {period}";
+                }
+                else // minute > 30
+                {
+                    timeString =
+                        $"{NumberToWords(60 - minute)} Minutes To {NumberToWords((hour + 1) % 12 == 0 ? 12 : (hour + 1) % 12)} {period}";
+                }
+            }
+            return timeString;
+        }
+
+        // Convert Date to Words ----------------------------------------------------------------------------------
+        private string ConvertDateToWords(DateTime date)
+        {
+            string dateString = "";
+
+            var numbersToWords = new Dictionary<int, string>
+            {
+                [1] = "First",
+                [2] = "Second",
+                [3] = "Third",
+                [4] = "Fourth",
+                [5] = "Fifth",
+                [6] = "Sixth",
+                [7] = "Seventh",
+                [8] = "Eighth",
+                [9] = "Ninth",
+                [10] = "Tenth",
+                [11] = "Eleventh",
+                [12] = "Twelfth",
+                [13] = "Thirteenth",
+                [14] = "Fourteenth",
+                [15] = "Fifteenth",
+                [16] = "Sixteenth",
+                [17] = "Seventeenth",
+                [18] = "Eighteenth",
+                [19] = "Nineteenth",
+                [20] = "Twentieth",
+                [21] = "Twenty-First",
+                [22] = "Twenty-Second",
+                [23] = "Twenty-Third",
+                [24] = "Twenty-Fourth",
+                [25] = "Twenty-Fifth",
+                [26] = "Twenty-Sixth",
+                [27] = "Twenty-Seventh",
+                [28] = "Twenty-Eighth",
+                [29] = "Twenty-Ninth",
+                [30] = "Thirtieth",
+                [31] = "Thirty-First"
+            };
+
+            string dayWord = date.ToString("dddd");
+            string dayNumber = numbersToWords[date.Day];
+            string dayNumberNow = date.ToString("dd");
+            string month = date.ToString("MMMM");
+            string year = date.ToString("yyyy");
+
+            dateString = $"{dayWord} The {dayNumber} of {month}, {year}"; // Full Date String
+
+            return dateString;
+        }
+    }
+}
